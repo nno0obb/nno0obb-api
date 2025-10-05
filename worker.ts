2245549b631a3c3f929d1639da25caf9163555e4
nno0obb <nno0obb@kakao.com>
2025-10-05 15:37:47 +0900

    @20251005.13

---
+++ worker.ts
@@ -0,0 +1,22 @@
+const routes = [
+    { pattern: /^\/api\/v1\//,        base: "https://nno0obb-api.fly.dev" },
+  ];
+  
+function getBase(pathname: string) {
+  for (const r of routes) if (r.pattern.test(pathname)) return r.base;
+  return null;
+}
+  
+export default {
+  async fetch(request: Request): Promise<Response> {
+    const url = new URL(request.url);
+    const base = getBase(url.pathname);
+    if (!base) return new Response("Not found", { status: 404 });
+  
+    const dest = new URL(base);
+    dest.pathname = url.pathname
+    dest.search = url.search;
+  
+    return fetch(dest.toString(), request);
+  },
+};
