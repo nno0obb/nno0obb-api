b05a5e8c103c1b22d11b95e4ed159d60592dfcd9
nno0obb <nno0obb@kakao.com>
2025-10-05 15:44:24 +0900

    @20251005.15

--- app/routers/v1/etc.py
+++ app/routers/v1/etc.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import JSONResponse, PlainTextResponse
 
 etc_v1_router = APIRouter(prefix="/api/v1", tags=["etc"])
 
@@ -7,6 +7,17 @@ etc_v1_router = APIRouter(prefix="/api/v1", tags=["etc"])
 @etc_v1_router.get("/ping")
 def get_ping():
     return PlainTextResponse("pong")
+
+
+@etc_v1_router.get("/info")
+def get_info():
+    return JSONResponse(
+        {
+            "app": {
+                "name": "nno0obb-api",
+            }
+        }
+    )
 
 
 def get_etc_v1_router():
