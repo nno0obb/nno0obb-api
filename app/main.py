2dd7166f1727dc912352d912cdb6c22ea7769c19
nno0obb <nno0obb@kakao.com>
2025-10-05 16:04:46 +0900

    @20251005.17

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.routers.v1.etc import get_etc_v1_router
+from app.routers.v1.root import get_v1_root_router
 
-app = FastAPI()
+app = FastAPI(
+    docs_url="/api/docs",
+    redoc_url=None,
+    openapi_url="/api/openapi.json",
+)
 
 
-app.include_router(get_etc_v1_router())
+app.include_router(get_v1_root_router(), prefix="/api")
 
 if __name__ == "__main__":
     config = uvicorn.Config(
--- app/routers/v1/root.py
+++ app/routers/v1/root.py
@@ -3,15 +3,15 @@ from functools import lru_cache
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse, PlainTextResponse
 
-etc_v1_router = APIRouter(prefix="/api/v1", tags=["etc"])
+v1_root_router = APIRouter(prefix="/v1", tags=["root"])
 
 
-@etc_v1_router.get("/ping")
+@v1_root_router.get("/ping")
 def get_ping():
     return PlainTextResponse("pong")
 
 
-@etc_v1_router.get("/info")
+@v1_root_router.get("/info")
 def get_info():
     return JSONResponse(
         {
@@ -23,5 +23,5 @@ def get_info():
 
 
 @lru_cache()
-def get_etc_v1_router():
-    return etc_v1_router
+def get_v1_root_router():
+    return v1_root_router
